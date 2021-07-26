a61df208394bfe344656f9bd3a4006257f95bf91
David Childerhose <davidoforange@outlook.com>
2021-07-26 15:56:47 +0900

    Made some changes to the populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
+const csv = require("csvtojson");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    csv()
+      .fromFile("cities.csv")
+      .then((data) => {
+        let result = 0;
+        for (let i = 0; i < data.length; i++) {
+          result += parseInt(data[i].population);
+        }
+        onFinished(result);
+      });
+  },
 };
